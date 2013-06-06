df5e94c70fe360a049434664bd9cab23a8c40ea5
Philip C. Wilson <philip@wilsonhut.com>
2013-06-05 23:02:07 -0500

    All parameters are optional
    As before:
    $.defer(myFunction, myDelay, myFunctionParameter);
    $.defer(myFunction, myDelay);
    Now, additionally:
    $.defer(myFunction);
    $.defer(myDelay);
    $.defer();

--- src/jquery.defer.js
+++ src/jquery.defer.js
@@ -1,31 +1,35 @@
-(function ($) {
+;(function ($, undefined) {
     var slice = Array.prototype.slice;
-    $.defer = function (func, delay) {
-        var args = slice.call(arguments, 2),
+    $.defer = function ( /*[func] [, delay] [, funcParameters]*/ ) {
+        var args = arguments,
+            isFunc = $.isFunction(args[0]),
+            func = isFunc ? args[0] : $.noop,
+            delay = (isFunc ? args[1] : args[0]) || 0,
+            funcArgs = isFunc ? slice.call(args, 2) : undefined,
             isCancelled = false,
-            cancel = function (reject /* = true */) {
+            cancel = function (reject /* = true */ ) {
                 clearTimeout(timerId);
                 isCancelled = true;
                 if ((!arguments.length || reject) && deferred.state() === "pending") {
-                    deferred.reject(null, promise, args);
+                    deferred.reject(null, promise, funcArgs);
                 }
             },
             deferred = $.Deferred(),
             timerId = setTimeout(function () {
-                deferred.notify(promise, args);
+                deferred.notify(promise, funcArgs);
                 if (isCancelled) {
                     return;
                 }
                 try {
-                    var result = func.apply(this, args);
-                    deferred.resolve(result, promise, args);
+                    var result = func.apply(this, funcArgs);
+                    deferred.resolve(result, promise, funcArgs);
                 } catch (e) {
-                    deferred.reject(e, promise, args);
+                    deferred.reject(e, promise, funcArgs);
                 }
             }, delay),
-            promise = deferred.promise();
-
-        promise.cancel = cancel;
+            promise = $.extend(deferred.promise(), {
+                cancel: cancel
+            });
         return promise;
     };
 })(jQuery);

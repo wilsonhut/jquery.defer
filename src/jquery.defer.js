a27478f29b2f273345213b5a40848f96a7e3cbcb
wilsonhut <github@wilsonhut.com>
2013-06-20 08:44:09 -0500

    arguments to .apply have to be an array for IE8 and older, at least.

--- src/jquery.defer.js
+++ src/jquery.defer.js
@@ -1,35 +1,35 @@
-;(function ($, undefined) {
+;(function($) {
     var slice = Array.prototype.slice;
-    $.defer = function ( /*[func] [, delay] [, funcParameters]*/ ) {
-        var args = arguments,
-            isFunc = $.isFunction(args[0]),
-            func = isFunc ? args[0] : $.noop,
-            delay = (isFunc ? args[1] : args[0]) || 0,
-            funcArgs = isFunc ? slice.call(args, 2) : undefined,
-            isCancelled = false,
-            cancel = function (reject /* = true */ ) {
-                clearTimeout(timerId);
-                isCancelled = true;
-                if ((!arguments.length || reject) && deferred.state() === "pending") {
-                    deferred.rejectWith(null, funcArgs);
-                }
-            },
-            deferred = $.Deferred(),
-            timerId = setTimeout(function () {
-                deferred.notifyWith(promise, funcArgs);
-                if (isCancelled) {
-                    return;
-                }
-                try {
-                    var result = func.apply(this, funcArgs);
-                    deferred.resolveWith(result, funcArgs);
-                } catch (e) {
-                    deferred.rejectWith(e, funcArgs);
-                }
-            }, delay),
-            promise = $.extend(deferred.promise(), {
-                cancel: cancel
-            });
-        return promise;
-    };
+	$.defer = function( /*[func] [, delay] [, funcParameters]*/) {
+		var args = arguments,
+		    isFunc = $.isFunction(args[0]),
+		    func = isFunc ? args[0] : $.noop,
+		    delay = (isFunc ? args[1] : args[0]) || 0,
+		    funcArgs = isFunc ? slice.call(args, 2) : [],
+		    isCancelled = false,
+		    cancel = function(reject /* = true */) {
+			    clearTimeout(timerId);
+			    isCancelled = true;
+			    if ((!arguments.length || reject) && deferred.state() === "pending") {
+				    deferred.rejectWith(null, funcArgs);
+			    }
+		    },
+		    deferred = $.Deferred(),
+		    timerId = setTimeout(function() {
+			    deferred.notifyWith(promise, funcArgs);
+			    if (isCancelled) {
+				    return;
+			    }
+			    try {
+				    var result = func.apply(this, funcArgs);
+				    deferred.resolveWith(result, funcArgs);
+			    } catch(e) {
+			    	deferred.rejectWith(e, funcArgs);
+			    }
+		    }, delay),
+		    promise = $.extend(deferred.promise(), {
+			    cancel: cancel
+		    });
+		return promise;
+	};
 })(jQuery);
